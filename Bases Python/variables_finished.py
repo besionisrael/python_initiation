e4c281f7cdbc3da2c17de24af6637d757c665cce
besionisrael <sion.israel.pro@gmail.com>
2021-09-12 02:46:38 +0100

    Maj on 12092021

--- Bases Python/variables_finished.py
+++ Bases Python/variables_finished.py
@@ -112,6 +112,51 @@ maliste[2] = 'modif'
 print(maliste)
 # Je modifie un élément (celui qui est en 3e position !)
 
+#Serie de fonction
+ma_liste = []
+ma_liste.append(1) # ajoute un élément à la liste
+print(ma_liste)
+print(ma_liste[0])
+ma_liste.index(1) # retourne l’index d’une valeur 
+ma_liste[0] = 2 # ajoute un élément à la liste
+del ma_liste[0]  # supprimer un entrée avec son index 
+ma_liste.remove(1)  # supprimer un entrée avec sa valeur ( la premiere trouvée ) 
+ma_liste.reverse()  # inverse la liste
+ma_liste.count(1)  # compte le nombre d’occurence d’une valeur
+ma_liste_imbrique = [1, [2, 3 ]] # liste imbriqué
+print(type(ma_liste ))
+
+#Copier une liste
+##Copie par référence
+y = [1,2,3]
+x = y # Attention ici x possède la même référence que y, ils sont donc lié 
+y[0] = 4
+print(x)
+
+##Copie par valeur
+x = [1,2,3]
+y = x[:] # copie par valeur et non pas par référence
+y[0] = 9
+print(x, y)
+
+#Transformation String en liste
+ma_chaine = "Nicolas:Haziza:Toulouse"
+resultat = ma_chaine.split(":")
+print(resultat)
+
+#Transformation liste en string
+ma_liste = ['Nicolas', 'Haziza', 'Toulouse']
+resultat = ':'.join(ma_liste)
+print(resultat)
+
+#Extension de liste  
+ma_liste = ['Nicolas', 'Haziza']
+ma_liste2 = ['Rennes', 'Toulouse']
+ma_liste.extend(ma_liste2)
+print(ma_liste)
+
+
+
 #Déclarer un tuple sans la possibilité de modifier son contenu
 montuple = (0, 1, 1, 2, 2, 3, 5, 8, 13)
 print(montuple)
@@ -159,6 +204,34 @@ print(berti)
 #Effacer une clé et la valeur qui lui est associée
 del berti['instruments']
 print(berti)
+
+#Récupérer une valeur depuis sa clé
+print(dico.get("computer"))
+print(dico["computer"])
+
+
+#Supprimer une entrée
+del dico["computer"]
+
+#Récupérer la listes des clés
+print(dico.keys())
+
+#Récupérer la listes des valeurs
+print(dico.values())
+
+
+#Récupérer la listes des clés/valeurs
+print(dico.items())
+
+
+
+#Copier un dictionnaire par référene
+dico_ref = dico
+
+#Copier un dictionnaire par valeur
+dico_copy =  dico.copy()
+
+
 
 
 
--- test.py
+++ test.py
@@ -1,11 +1,8 @@
-
-
-
-x = 100
-y = 10000
-
-
-if x > y:
-    print("X est plus grand que Y")
-else:
-    print("X est inferieur ou égale à Y")
+t1 = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+t2 = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin','Juillet',  'Août',  'Septembre',  'Octobre',  'Novembre','Décembre']
+list = []
+newlist = []
+for numero in range(0,len(t1)):
+    newlist.append(t2[numero])
+    newlist.append(t1[numero])
+print(newlist)
