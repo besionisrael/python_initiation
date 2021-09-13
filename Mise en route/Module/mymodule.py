7e53a0ec205eb45ae0d7e87e4e6628a5c5d8f785
besionisrael <sion.israel.pro@gmail.com>
2021-09-13 07:26:52 +0100

    Update

--- Mise en route/Module/mymodule.py
+++ Mise en route/Module/mymodule.py
@@ -2,7 +2,7 @@
 def dire_bonjour(nom):
     return 'Bonjour {qui}'.format(qui=nom)
 
+print (dire_bonjour('Pascal'))
 
-if __name__ == '__main__':
-    print (dire_bonjour('Pascal'))
+
 
--- test.py
+++
@@ -1,8 +0,0 @@
-t1 = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-t2 = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin','Juillet',  'Août',  'Septembre',  'Octobre',  'Novembre','Décembre']
-list = []
-newlist = []
-for numero in range(0,len(t1)):
-    newlist.append(t2[numero])
-    newlist.append(t1[numero])
-print(newlist)
