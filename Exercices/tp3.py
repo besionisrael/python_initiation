f88d846bd7d607851a58f1e7f2f3790e75ed583b
Sion Israel <sion.israel.pro@gmail.com>
2022-10-08 16:28:09 -0400

    endtp

--- Exercices/tp3.py
+++
@@ -1,8 +0,0 @@
-#TP Battle Game
-#Consultez les slides
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    main()
---
+++ Exercices/tpEndSession.py
@@ -0,0 +1,30 @@
+# Proposer un programme python qui propose un formulaire composé des 
+# entrées Prénom, Age et Taille ainsi qu'une liste des donnée telle que présentée
+# dans la figure.
+# Le formulaire comprend quatre boutons:
+# Enregistrer, Afficher, charger et Reconstituer qui sont rattachés à quatres opérations principales:
+# le bouton Enregistrer permet de sauvegarder les informations saisies au clavier
+# par l'utilisateur dans un dictionnaire. 
+# Dans le dictionnaire, le nom de l’élève servira de clé d’accès, et les valeurs seront constituées de tuples (âge, taille), dans lesquels l’âge sera exprimé en années (donnée de type entier), et la taille en mètres (donnée de type réel.
+# Le bouton Afficher permet d'afficher le contenu du dictionnaire dans un message box sur un format :
+# "{Nom} a {âge} ans et {taille} m"
+
+# Le bouton "Enregistrer dans fichier" permet d'enregistrer le dictionnaire résultant dans un fichier texte.
+# Chaque ligne de votre fichier texte correspondra à un élément du dictionnaire. Elle sera
+# formatée de manière à bien séparer :
+# - la clé et la valeur (c’est-à-dire le nom de la personne, d’une part, et l’ensemble : « âge +
+# taille », d’autre part ;
+# - dans l’ensemble « âge + taille », ces deux données numériques.
+# Vous utiliserez donc deux caractères séparateurs différents, par exemple « @ » pour séparer la clé et la valeur, et « » pour séparer les données constituant cette valeur :
+# Henry@28#1.67
+# Thierry@29#1.8
+# Delphine@19#1.71
+# Le bouton "Reconstituer dictionnaire  " permet de reconstituer le dictionnaire à partir du fichier correspondant.
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
